2ab24198b6888c4629bd996b141c5072a00d233c
Ed Yu <ed.yu@yahoo.com>
2013-02-14 16:46:04 -0800

    Handle extra features

--- reverse_polish.rb
+++ reverse_polish.rb
@@ -17,36 +17,72 @@ class ReversePolish
   def run
     puts "type 'q' to quit"
     while true
+      quit = false
       print("> ")
       line = gets.chomp.strip
-      case line
-      when "q"
-        break
-      when /^[-+]?(\d*\.\d+|\d+\.\d*)$/
-        op = line.to_f
-        @stack.push op
-        puts op
-      when /^[-+]?\d+$/
-        op = line.to_i
-        @stack.push op
-        puts op
-      when /^[+-\/\*]$/
-        if @stack.length < 2
-          puts "not enough operands"
-          next
+      tokens = line.split
+      tokens.each do |token|
+        begin
+          case token
+          when "q"
+            puts "goodbye"
+            exit
+          when /^[-+]?(\d*\.\d+|\d+\.\d*)$/
+            op = token.to_f
+            @stack.push op
+            puts op
+          when /^[-+]?\d+$/
+            op = token.to_i
+            @stack.push op
+            puts op
+          when /^[+-\/\*]$/
+            if @stack.length < 2
+              puts "not enough operands"
+              next
+            end
+            op2 = @stack.pop
+            op1 = @stack.pop
+            result = op1.send token.to_sym, op2
+            @stack.push result
+            puts result
+          when /sum/i
+            result = @stack.inject(:+) || 0
+            @stack.clear
+            @stack.push result
+            puts result
+          when /!/
+            if @stack.length < 1
+              puts "not enough operands"
+              next
+            end
+            op = @stack.pop
+            result = (1..op.to_i).inject(:*) || 1
+            @stack.push result
+            puts result
+          else
+            # ignore
+            if Math.respond_to? token.downcase.to_sym
+              if @stack.length < 1
+                puts "not enough operands"
+                next
+              end
+              op = @stack.pop
+              result = Math.send token.to_sym, op
+              @stack.push result
+              puts result
+            elsif Math.constants.include?(token.upcase.to_sym)
+              op = Math.const_get(token.upcase.to_sym)
+              @stack.push op
+              puts op
+            else
+              puts "you must input a number or a
+            end
+          end
+        rescue => e
+          puts e.message
         end
-        op2 = @stack.pop
-        op1 = @stack.pop
-        result = op1.send line.to_sym, op2
-        @stack.push result
-        puts result
-      when //
-        # ignore
-      else
-        puts "you must input a number or an operand (+ - * /)"
       end
     end
-    puts "goodbye"
   end
 end
 
